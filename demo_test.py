a769c320b51afd0f05e4e62d4065cdbb4c6f5a08
R-Irina <94069847+R-Irina@users.noreply.github.com>
2022-02-14 01:36:56 +0300

    Add files via upload

---
+++ demo_test.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+from xgboost import XGBRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+import numpy as np
+
+df = pd.read_csv('housing.csv')
+
+if st.button('Отобразить первые десять строк'):
+    st.write(df.head(10))
+
+n_size = st.selectbox('Размер тестовой выборки:', [10, 15, 20, 25, 30, 35])
+
+if st.button('Обучить модель'):
+    X_train, X_test, y_train, y_test = train_test_split(df.drop('MEDV', axis=1),
+                                                        df['MEDV'],
+                                                        test_size=n_size/100,
+                                                        random_state=2100)
+    st.write('Разделили данные и передали в обучение')
+    regr_model = XGBRegressor()
+    regr_model.fit(X_train, y_train)
+    pred = regr_model.predict(X_test)
+    st.write('Обучили модель, MAE = ' + str(mean_absolute_error(y_test, pred)))
+    st.write(pd.DataFrame({'y_test':y_test, 'pred':pred}))
